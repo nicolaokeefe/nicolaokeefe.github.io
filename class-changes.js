afd7ba48966f1a680a036c19e6a316d5d1a10548
nicolaokeefe <nicolaokeefe02@gmail.com>
2025-05-30 09:20:09 +1000

    upload 3

---
+++ class-changes.js
@@ -0,0 +1,13 @@
+const classChanges = [
+  { time: 5, target: ".quote", add: "update" },
+  { time: 8, target: ".quote", remove: "update" },
+  { time: 9, target: ".page", add: "page.update" },
+
+  { time: 9, target: ".changeme2", add: "show" },
+  { time: 16, target: ".changeme2", remove: "show" },
+  { time: 16, target: ".changeme2", add: "hidden" },
+
+  { time: 16, target: ".changeme3", add: "show" },
+  { time: 28, target: ".changeme3", remove: "show" },
+  { time: 28, target: ".changeme3", add: "hidden" }
+];
